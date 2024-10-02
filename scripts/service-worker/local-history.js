57432e550a20ffb51ba2ee867e6bce2cdbbd7201
bagzaru <bagzaru@naver.com>
2024-10-02 14:34:02 +0900

    service-worker 분할

---
+++ scripts/service-worker/local-history.js
@@ -0,0 +1,25 @@
+//localHistoryDB에 저장할 것들
+const historyFrame = {
+    visitTime: "",
+    title: "",
+    url: "",
+    timeOnPage: "",
+    pageData: ""
+}
+
+async function loadLocalHistoryDB() {
+    const key = 'localHistoryDB';
+    let localHistoryData = [];
+
+    //load하여 localHistoryData 배열에 저장.
+    localHistoryData = await new Promise((resolve, reject) => {
+        chrome.storage.local.get([key], (result) => {
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            resolve(result[key]);
+        })
+    });
+
+    return localHistoryData;
+}
