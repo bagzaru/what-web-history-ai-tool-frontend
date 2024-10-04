9cd6205e973ecfbcc3fe87776304e9fb771061ec
bagzaru <bagzaru@naver.com>
2024-10-04 12:42:46 +0900

    message Handler 모듈화

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,24 +1,7 @@
-import { post } from "./service-worker/dataSender.js";
-import { createHistoryBody } from "./service-worker/localhistory.js";
+import { messageHandler } from "./service-worker/messageHandler.js";
+
 console.log("service-worker on");
 
-//content, popup 등에서 전송된 Message 값 처리
-function messageHandler(message, sender, sendResponse) {
-    if (message.action === "DOM_LOADED") {
-        console.log("service-worker msg Handler: DOM LOADED!!")
-        //TODO: 텍스트 데이터 서버에 전송
-        const path = "/createHistory";
-        const body = createHistoryBody((new Date()).getTime, message.data.title, message.data.url, 0, message.data.pageData);
-
-        post(path, body)
-            .then((data) => console.log("POST: history created"))
-            .catch((err) => console.log("POST: err" + err));
-    }
-    else {
-        sendResponse({ k: message.action });
-        return false;
-    }
-}
 chrome.runtime.onMessage.addListener(messageHandler);
 
 //onCommitted, onCompleted 확인
