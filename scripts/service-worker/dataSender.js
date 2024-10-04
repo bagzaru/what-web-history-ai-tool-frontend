86bac6e244b4766b337fed3a42813018a85c23be
bagzaru <bagzaru@naver.com>
2024-10-04 13:43:16 +0900

    put 함수 추가

--- scripts/service-worker/dataSender.js
+++ scripts/service-worker/dataSender.js
@@ -1,9 +1,14 @@
 const defaultHost = "seokjin.url";
 
-async function post(path, body, headers = {}) {
+function getURLfromPath(path) {
     const url = /^https?:\/\//.test(path)
         ? path
         : `https://${defaultHost}/${path}`;
+    return url;
+}
+
+async function post(path, body, headers = {}) {
+    const url = getURLfromPath(path);
     const options = {
         method: "POST",
         headers: {
@@ -24,6 +29,28 @@ async function post(path, body, headers = {}) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
 }
+async function put(path, body, headers = {}) {
+    const url = getURLfromPath(path);
+    const options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            ...headers
+        },
+        body: JSON.stringify(body)
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (response.ok) {
+        return data;
+    }
+    else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+}
 
 
-export { post };
+export { post, put };
