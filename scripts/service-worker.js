2cadfa7cb7fc436e8f5a3fe1ef6994ea71c97537
bagzaru <bagzaru@naver.com>
2024-10-09 11:39:20 +0900

    service-worker: tabNavigation 이벤트 테스트 함수 주석처리

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -10,16 +10,16 @@ chrome.windows.onFocusChanged.addListener(windowFocusChangeHandler);
 
 /* 이하는 테스트용 코드 */
 
-//onCommitted, onCompleted 확인
-chrome.webNavigation.onCommitted.addListener(function (details) {
-    console.log(`onCommitted 완료, url: ${details.url}`);
-}/*, { url: [{ urlMatches: 'https://example.com/*' }] }*/);
+// //onCommitted, onCompleted 확인
+// chrome.webNavigation.onCommitted.addListener(function (details) {
+//     console.log(`onCommitted 완료, url: ${details.url}`);
+// }/*, { url: [{ urlMatches: 'https://example.com/*' }] }*/);
 
-chrome.webNavigation.onCompleted.addListener((details) => {
-    console.log(`onCompleted 완료, url: ${details.url}`);
-});
+// chrome.webNavigation.onCompleted.addListener((details) => {
+//     console.log(`onCompleted 완료, url: ${details.url}`);
+// });
 
-//방문기록 업데이트 확인
-chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-    console.log(`service-worker: onHistoryStateUpdated 완료, uuid: ${details.documentId}, url: ${details.url}`);
-});
+// //방문기록 업데이트 확인
+// chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+//     console.log(`service-worker: onHistoryStateUpdated 완료, uuid: ${details.documentId}, url: ${details.url}`);
+// });
