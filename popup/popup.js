603d554ccebb5c03cf2a62eefa5990f7e669e0ce
bagzaru <bagzaru@naver.com>
2024-10-10 23:50:53 +0900

    popup div 크기 조정

--- popup/popup.js
+++ popup/popup.js
@@ -15,21 +15,3 @@ window.onload = function () {
             }
         })
 }
-
-const popupText = document.querySelector('#popupText');
-const logText = document.querySelector('#debugLog');
-
-popupText.innerText = '쿠구궁...';
-
-chrome.runtime.sendMessage({ type: '' }, (response) => {
-    popupText.innerText = response.text;
-});
-
-chrome.storage.local.get(['localHistoryDB'], (result) => {
-    if (result.localHistoryDB) {
-        logText.innerText = result.localHistoryDB.map((x) => x.title).join('\n');
-    }
-    else {
-        logText.innerHTML = "데이터 탐색 실패";
-    }
-})
