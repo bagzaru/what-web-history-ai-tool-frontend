c2686979c18add09c1097c147f08d29b544060ea
bagzaru <bagzaru@naver.com>
2024-10-26 16:04:18 +0900

    seunghoon: DOM-Distiller 적용

--- scripts/service-worker/messageHandler.js
+++ scripts/service-worker/messageHandler.js
@@ -7,28 +7,41 @@ function messageHandler(message, sender, sendResponse) {
         console.log("service-worker msg Handler: DOM LOADED!!");
 
         //데이터 서버에 전송할 데이터 제작
-        const data = {
+        let data = {
             title: message.data.title,
             url: message.data.url,
             content: message.data.pageData
         }
 
-        //텍스트 데이터 서버에 전송
-        networkManager.post.saveHistory(data)
-            .then(async () => {
-                try {
-                    //24.10.16: 현재 자동 키워드 추출 사용하지 않음(비용 문제)
-                    // - 실제 릴리즈 시에는 사용할 예정
-                    //await server.put.extractKeywords(data.url);
-                    //console.log("extractKeywords 성공적");
-                }
-                catch (e) {
-                    console.log(`extractKeywords error: ${e.message}`);
-                }
-            })
-            .catch((e) => {
-                console.log(`saveHistory Error: ${e.message}`);
+        //DOM Distiller를 통해 요약된 데이터 추출
+        chrome.tabs.sendMessage(
+            sender.tab.id,
+            { action: "DOM_DISTILLER_EXTRACT", data: {} },
+            (response) => {
+                data.title = response.data.title;
+                data.content = response.data.content;
+                console.log("Distilled Data:" + data.title + ", content: " + data.content);
+
+                //텍스트 데이터를 서버에 전송
+                networkManager.post.saveHistory(data)
+                    .then(async () => {
+                        try {
+                            //24.10.16: 현재 자동 키워드 추출 사용하지 않음(비용 문제)
+                            // - 실제 릴리즈 시에는 사용할 예정
+                            //await server.put.extractKeywords(data.url);
+                            //console.log("extractKeywords 성공적");
+                        }
+                        catch (e) {
+                            console.log(`extractKeywords error: ${e.message}`);
+                        }
+                    })
+                    .catch((e) => {
+                        console.log(`saveHistory Error: ${e.message}`);
+                    });
             });
+
+        //TODO: Response가 오지 않거나, Distill 실패 경우가 있는지, 있는 경우에 대한 대처
+        //TODO: Distiller를 사용하는 경우와 사용하지 않는 경우 나누기
     }
     else if (message.action === "NETWORK_STATE_CHANGED") {
         //popup에서 serverState값이 바뀌었을 때에 대한 대응
