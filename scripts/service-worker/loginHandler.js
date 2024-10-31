ef9ea5b45262d5a6347a284f41633135aabce4d3
lsh5784 <dltjdgh5784@gmail.com>
2024-10-31 21:13:48 +0900

    토큰 전송에 헤더를 이용하는 방법으로 변경

--- scripts/service-worker/loginHandler.js
+++ scripts/service-worker/loginHandler.js
@@ -3,44 +3,45 @@ async function loginHandler(details) {
     try {
         if (
             details.url.includes("/api/auth/oauth2/google") &&
-            details.statusCode === 200 &&
-            details.initiator === "https://accounts.google.com"
+            details.statusCode === 200
         ) {
-            const response = await fetch(details.url);
-            if (!response.ok) {
-                console.error("토큰 가져오기 실패, 상태 코드:", response.status);
-                return;
-            }
+            // console.log("OAuth2 요청 완료:", details);
+            // console.log("헤더", details.responseHeaders);
+            
+            // 헤더에서 JWT토큰 추출
+            const responseHeaders = details.responseHeaders;
+            const tokenHeader = responseHeaders?.find(
+                (header) => header.name.toLowerCase() === "authorization"
+            );
+
+            if (tokenHeader) {
+                const token = tokenHeader.value.split("Bearer ")[1];
+
+                if (token) {
+                    // Chrome 스토리지에 토큰 저장
+                    chrome.storage.local.set({ jwtToken: token }, () => {
+                        if (chrome.runtime.lastError) {
+                            console.error("토큰 저장 오류:", chrome.runtime.lastError);
+                        } else {
+                            console.log("JWT 토큰 저장 성공:", token);
     
-            const data = await response.json();
-            const token = data.token;
-
-            // console.log("OAuth2 요청완료", details);
-
-            if (token) {
-                // Chrome 스토리지에 토큰 저장
-                chrome.storage.local.set({ jwtToken: token }, () => {
-                    if (chrome.runtime.lastError) {
-                        console.error("토큰 저장 오류:", chrome.runtime.lastError);
-                    } else {
-                        console.log("JWT 토큰 저장 성공:", token);
-
-                        // 팝업창 닫기
-                        chrome.windows.getAll({ windowTypes: ["popup"] }, (windows) => {
-                            windows.forEach((win) => {
-                                chrome.tabs.query({ windowId: win.id }, (tabs) => {
-                                    tabs.forEach((tab) => {
-                                        if (tab.url.includes("capstonepractice.site")) {
-                                            chrome.windows.remove(win.id, () => {
-                                            console.log("팝업 창이 닫혔습니다:", tab.url);
-                                            });
-                                        }
+                            // 팝업창 닫기
+                            chrome.windows.getAll({ windowTypes: ["popup"] }, (windows) => {
+                                windows.forEach((win) => {
+                                    chrome.tabs.query({ windowId: win.id }, (tabs) => {
+                                        tabs.forEach((tab) => {
+                                            if (tab.url.includes("capstonepractice.site")) {
+                                                chrome.windows.remove(win.id, () => {
+                                                console.log("팝업 창이 닫혔습니다:", tab.url);
+                                                });
+                                            }
+                                        });
                                     });
                                 });
                             });
-                        });
-                    }
-                });
+                        }
+                    });
+                }
             }
         }
     } catch (error) {
