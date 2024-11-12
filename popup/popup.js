66346f1f4de70a30aa27a5341144aad2d201f490
lsh5784 <dltjdgh5784@gmail.com>
2024-11-12 23:47:47 +0900

    임시로 쓸 이전 popup.html 에서 로그인 관련 요소 삭제

--- popup/popup.js
+++ popup/popup.js
@@ -116,27 +116,27 @@ function loadHistoryData(orderBy) {
         }
     });
 
-    const googleLoginButton = document.getElementById("google-login-button");
-    googleLoginButton.addEventListener('click', () => {
-        const backendLoginUrl = "https://capstonepractice.site/oauth2/authorization/google";
-        // 팝업 창을 열어 백엔드 로그인 url로 이동
-        chrome.windows.create(
-            {
-                url: backendLoginUrl,
-                type: "popup",
-                width: 500,
-                height: 700,
-            }
-          );
-    });
+    // const googleLoginButton = document.getElementById("google-login-button");
+    // googleLoginButton.addEventListener('click', () => {
+    //     const backendLoginUrl = "https://capstonepractice.site/oauth2/authorization/google";
+    //     // 팝업 창을 열어 백엔드 로그인 url로 이동
+    //     chrome.windows.create(
+    //         {
+    //             url: backendLoginUrl,
+    //             type: "popup",
+    //             width: 500,
+    //             height: 700,
+    //         }
+    //       );
+    // });
 
-    const showTokenButton = document.getElementById("show-token");
-    showTokenButton.addEventListener('click', () => {
-        chrome.storage.local.get((result) => console.log(result));
-    });
+    // const showTokenButton = document.getElementById("show-token");
+    // showTokenButton.addEventListener('click', () => {
+    //     chrome.storage.local.get((result) => console.log(result));
+    // });
 
-    const logoutButton = document.getElementById("logout-button");
-    logoutButton.addEventListener('click', async () => {
-        await logoutHandler();
-    })
+    // const logoutButton = document.getElementById("logout-button");
+    // logoutButton.addEventListener('click', async () => {
+    //     await logoutHandler();
+    // })
 }
