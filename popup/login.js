3131e82b505573cfce9809597cd4d564d6349c83
lsh5784 <dltjdgh5784@gmail.com>
2024-11-12 23:50:11 +0900

    로그인 페이지에서 로그인 상태 처리 구현 완료

--- popup/login.js
+++ popup/login.js
@@ -1,4 +1,7 @@
 const googleLoginButton = document.getElementById("google-login-button");
+
+// 로그인 버튼을 눌렀을 때, 로그인 요청 message를 service-worker.js에 보냄.
+// new Promise를 사용해 응답이 올 때까지 기다림.
 googleLoginButton.addEventListener('click', async() => {
     try {
         const response = await new Promise((resolve, reject) => {
