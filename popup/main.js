ec362b641fb22b1f03639618e2ebc4e797a68de3
lsh5784 <dltjdgh5784@gmail.com>
2024-12-04 16:11:54 +0900

    토큰 만료 체크 로직 추가

--- popup/main.js
+++ popup/main.js
@@ -36,8 +36,18 @@ document.addEventListener("DOMContentLoaded", function () {
             const email_text = document.getElementById("user-name");
             email_text.innerText = result.user_email;
             email_text.title = result.user_email;
-            await refreshToken();
+            if (isTokenExpired(result.jwtToken)){
+                await refreshToken();
+            } else {
+                console.log("토큰이 만료되지 않았습니다.");
+            }
         } else {
+            chrome.notifications.create({
+                type: "basic",
+                iconUrl: "../icon.png",
+                title: "로그아웃 알림",
+                message: "로그아웃 상태입니다. 로그인해주세요."
+            });
             console.log("jwtToken이 존재하지 않습니다:");
             // 로그아웃 상태일 시 사용자 정보 박스 및 로그아웃 버튼이 보이지 않음
             document.getElementById("login-info").style.display = "none";
@@ -111,11 +121,43 @@ async function refreshToken() {
         });
         console.log("response data:", response);
         if (response.data === true) {
+            chrome.notifications.create({
+                type: "basic",
+                iconUrl: "../icon.png",
+                title: "로그인 알림",
+                message: "로그인 세션이 자동 갱신되었습니다."
+            });
             console.log("From main.js : Refreshing Token Success");
         } else {
             console.log("From main.js : Refreshing Token Failed");
         }
     } catch (error) {
         console.error("Refreshing request failed:", error);
+    }
+}
+
+//JWT 디코딩 함수
+function decodeJWT(jwtToken) {
+    const paylaodBase64Url = jwtToken.split('.')[1];
+    const payloadBase64 = paylaodBase64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const payloadJson = atob(payloadBase64);
+    return JSON.parse(payloadJson);
+}
+
+//토큰 만료 체크 함수
+function isTokenExpired(jwtToken){
+    try {
+        const payload = decodeJWT(jwtToken);
+        const currentTime = Date.now();
+        const expirationTime = payload.exp * 1000;
+        const expirationDate = new Date(expirationTime);
+        console.log("token expiring date:",expirationDate.toLocaleString());
+        const oneHourInMilliseconds = 60 * 60 * 1000;
+
+        //만료되었거나 만료까지 1시간 미만일 때, true를 리턴
+        return currentTime >= expirationTime || (expirationTime - currentTime) < oneHourInMilliseconds;
+    } catch (error) {
+        console.error("Invalid token:", error);
+        return true; //토큰 파싱 실패 시 만료된 것으로 간주
     }
 }
