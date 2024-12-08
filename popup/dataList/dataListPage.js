af681b5dbfc4187e8d6b2fbc6124497713d29e8a
lsh5784 <dltjdgh5784@gmail.com>
2024-12-09 02:30:05 +0900

    카테고리별 분류, 데이터 리스트 페이징 적용

--- popup/dataList/dataListPage.js
+++ popup/dataList/dataListPage.js
@@ -1,14 +1,41 @@
 import renderArticles from "../renderArticles/renderArticles.js";
 
-const articleContainer = document.getElementById('article-container');
+let currentQueries = {
+    startTime: '',
+    endTime: '',
+    domain: '',
+    category: '',
+    page: '',
+    size: '',
+    sortBy: '',
+    sortOrder: '',
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
     await renderMenu();
+    loadDataList(currentQueries);
+    addMovePageButton();
 });
 
-function loadDataList() {
+// 방문기록 데이터 렌더링
+function loadDataList({startTime, endTime, domain, category, page, size, sortBy, sortOrder}) {
+    const articleContainer = document.getElementById('article-container');
+    const pageNumList = document.querySelector('.page-num-list');
     console.log("데이터 로드 시도");
-    chrome.runtime.sendMessage({ senderName: "popup", action: "GET_ALL_DATA_LIST" }, (response) => {
+    chrome.runtime.sendMessage({ 
+            senderName: "popup", 
+            action: "GET_ALL_DATA_LIST",
+            queries: {
+                startTime: startTime,
+                endTime: endTime,
+                domain: domain,
+                category: category,
+                page: page,
+                size: size,
+                sortBy: sortBy,
+                sortOrder: sortOrder
+            } 
+        }, (response) => {
         const data = response.data;
         if (data === null) {
             console.log("데이터 요청 실패: " + response.message);
@@ -17,10 +44,20 @@ function loadDataList() {
             console.log("데이터 요청 성공");
         }
         console.log("데이터: " + JSON.stringify(data));
-        const articles = renderArticles(data);
-        articleContainer.innerHTML = "";
+        resetData();
+        const articles = renderArticles(data.content);
         articleContainer.appendChild(articles);
+        if (pageNumList.children.length === 0) {
+            renderPaging(data.totalPages);
+        }
+        const elementTop = articleContainer.offsetTop - 70;
+        window.scrollTo({ top: elementTop, behavior:'smooth'});
     });
+}
+
+function resetData(){
+    const articleContainer = document.getElementById('article-container');
+    articleContainer.innerHTML = "";
 }
 
 // 카테고리를 가져오는 함수
@@ -40,6 +77,7 @@ function getCategories() {
 async function renderMenu() {
     // `chrome.storage.sync`에서 리스트 가져오기
     const list = await getCategories();
+    list.unshift('전체');
 
     // 메뉴 리스트 요소 가져오기
     const menuList = document.querySelector('.menu-list');
@@ -48,7 +86,10 @@ async function renderMenu() {
     list.forEach(item => {
         const listItem = document.createElement('li');
         listItem.textContent = item; // 항목 텍스트
-        listItem.onclick = () => alert(`You clicked: ${item}`); // 클릭 시 동작 추가
+        if (item === '전체'){
+            listItem.classList.add('active');
+        }
+        changeStyleCategory.call(listItem);
         menuList.appendChild(listItem);
     });
 
@@ -100,5 +141,149 @@ function smoothScroll(container, distance, duration) {
     requestAnimationFrame(animateScroll);
 }
 
+// 활성화 된 탭 표시
+function changeStyleCategory() {
+    this.addEventListener("click", function () {
+        const list = this.parentNode.children;
+        const firstChild = this.parentNode.firstElementChild;
+        for (let item of list) {
+            if (item.classList.contains("active")) {
+                item.classList.remove("active");
+            }
+        }
+        this.classList.add("active");
+        if (this === firstChild){
+            currentQueries.category = '';
+            currentQueries.page = 0;
+        } else {
+            currentQueries.category = this.textContent;
+            currentQueries.page = 0;
+        }
+        resetPaging();
+        loadDataList(currentQueries);
+        updateVisibleItems();
+    });
+}
 
-loadDataList();
+// 하단 페이징 메뉴를 렌더링하는 함수
+function renderPaging(totalPageNum) {
+    // 메뉴 리스트 요소 가져오기
+    const pageNumList = document.querySelector('.page-num-list');
+
+    for (let i = 0; i < totalPageNum; i++){
+        const listItem = document.createElement('li');
+        listItem.textContent = `${i + 1}`;
+        if (i === 0) {
+            listItem.classList.add('active');
+        }
+        changeStylePage.call(listItem);
+        pageNumList.appendChild(listItem);
+    }
+    updateVisibleItems();
+}
+
+// 활성화 된 페이지 번호 표시
+function changeStylePage() {
+    this.addEventListener("click", function () {
+        const list = Array.from(this.parentNode.children);
+        const clickedIndex = list.indexOf(this);
+        for (let item of list) {
+            if (item.classList.contains("active")) {
+                item.classList.remove("active");
+            }
+        }
+        this.classList.add("active");
+        currentQueries.page = clickedIndex;
+        loadDataList(currentQueries);
+        updateVisibleItems();
+    });
+}
+
+// 페이징 메뉴 초기화
+function resetPaging() {
+    const pageNumList = document.querySelector('.page-num-list');
+    pageNumList.innerHTML = "";
+}
+
+// 페이지 이동 버튼 기능 활성화
+function addMovePageButton() {
+    const prevPageButton = document.getElementById('prev-page');
+    const nextPageButton = document.getElementById('next-page');
+    
+    prevPageButton.addEventListener('click', () => {
+        const pageNumList = document.querySelector('.page-num-list');
+        const items = Array.from(pageNumList.children);
+        console.log("From prevButton items:", items);
+        const activeIndex = items.findIndex((item) => item.classList.contains('active'));
+        console.log("activeIndex:", activeIndex);
+        items[activeIndex].classList.remove('active');
+        items[activeIndex-1].classList.add('active');
+        currentQueries.page = activeIndex-1;
+        loadDataList(currentQueries);
+        updateVisibleItems();
+    });
+
+    nextPageButton.addEventListener('click', () => {
+        const pageNumList = document.querySelector('.page-num-list');
+        const items = Array.from(pageNumList.children);
+        console.log("From nextButton items:", items);
+        const activeIndex = items.findIndex((item) => item.classList.contains('active'));
+        console.log("activeIndex:", activeIndex);
+        items[activeIndex].classList.remove('active');
+        items[activeIndex+1].classList.add('active');
+        currentQueries.page = activeIndex+1;
+        loadDataList(currentQueries);
+        updateVisibleItems();
+    })
+}
+// 페이지 이동 버튼 리스너 해제
+function removeMovePageButton() {
+    const prevPageButton = document.getElementById('prev-page');
+    const nextPageButton = document.getElementById('next-page');
+    
+}
+
+function updateVisibleItems() {
+    const pageNumList = document.querySelector('.page-num-list');
+    const items = Array.from(pageNumList.children);
+    const activeIndex = items.findIndex((item) => item.classList.contains('active'));
+    const prevPageButton = document.getElementById('prev-page');
+    const nextPageButton = document.getElementById('next-page');
+
+    if (items.length <= 1) {
+        prevPageButton.style.opacity = '0';
+        prevPageButton.style.pointerEvents = 'none';
+        nextPageButton.style.opacity = '0';
+        nextPageButton.style.pointerEvents = 'none';
+    } else {
+        if (activeIndex === 0) {
+            prevPageButton.style.opacity = '0';
+            prevPageButton.style.pointerEvents = 'none';
+            nextPageButton.style.opacity = '1';
+            nextPageButton.style.pointerEvents = 'auto';
+        } else if (activeIndex === items.length - 1) {
+            prevPageButton.style.opacity = '1';
+            prevPageButton.style.pointerEvents = 'auto';
+            nextPageButton.style.opacity = '0';
+            nextPageButton.style.pointerEvents = 'none';
+        } else {
+            prevPageButton.style.opacity = '1';
+            prevPageButton.style.pointerEvents = 'auto';
+            nextPageButton.style.opacity = '1';
+            nextPageButton.style.pointerEvents = 'auto';
+        }
+    }
+
+    items.forEach((item, index) => {
+        if (activeIndex < 2) {
+            // activeIndex가 리스트 시작부 근처일 때
+            item.style.display = index < 5 ? "flex" : "none";
+        } else if (activeIndex >= items.length - 2) {
+            // activeIndex가 리스트 끝 근처일 때
+            item.style.display = index >= items.length - 5 ? "flex" : "none";
+        } else {
+            // activeIndex가 중간에 있을 때
+            item.style.display = index >= activeIndex - 2 && index <= activeIndex + 2 ? "flex" : "none";
+        }
+    });
+}
