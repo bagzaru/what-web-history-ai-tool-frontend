62b85ab5a934042c6cb5e1a23b754ea313c75613
lsh5784 <dltjdgh5784@gmail.com>
2024-12-12 03:18:36 +0900

    데이터 유효성 검사 부분 수정

--- scripts/service-worker/messageHandler/popup/onUpdateData.js
+++ scripts/service-worker/messageHandler/popup/onUpdateData.js
@@ -8,7 +8,7 @@ const onUpdateData = {
         //popup에서 데이터 업데이트를 요청했을 때
         console.log("UPDATE_HISTORY_DATA: 데이터 업데이트 요청" + JSON.stringify(message.data));
 
-        if (message.data.url === undefined || message.data.category === undefined || message.data.category === '변경안함') {
+        if ((!message.data.url) || (!message.data.category) || (message.data.category === '변경안함')) {
             console.log("필수 데이터 없음");
             return { data: null };
         }
