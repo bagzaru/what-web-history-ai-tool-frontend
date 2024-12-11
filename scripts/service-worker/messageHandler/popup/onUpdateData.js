6a0276a4b9f6ade5dc85752c5361b098ad4de00e
lsh5784 <dltjdgh5784@gmail.com>
2024-12-11 16:03:04 +0900

    데이터 수정 요청 message 리스너 추가

---
+++ scripts/service-worker/messageHandler/popup/onUpdateData.js
@@ -0,0 +1,24 @@
+import networkManager from "../../networking/networkManager.js";
+
+const onUpdateData = {
+    senderName: "popup",
+    eventName: "UPDATE_HISTORY_DATA",
+
+    listener: async function (message, sender) {
+        //popup에서 데이터 업데이트를 요청했을 때
+        console.log("UPDATE_HISTORY_DATA: 데이터 업데이트 요청" + JSON.stringify(message.data));
+
+        if (message.data.url === undefined || message.data.category === undefined || message.data.category === '변경안함') {
+            console.log("필수 데이터 없음");
+            return { data: null };
+        }
+
+
+        const response = await networkManager.put.updateHistoryData(message.data);
+
+        console.log("UPDATE_HISTORY_DATA: 데이터 업데이트 성공");
+        return { data: response };
+    }
+}
+
+export default onUpdateData;
